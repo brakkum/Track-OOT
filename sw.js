85bdf6b2f1c1164df0588ef89910e3e8255c4126
Denis Peters <denis.peters@udo.edu>
2018-11-09 20:38:57 +0100

    add files to service worker

--- sw.js
+++ sw.js
@@ -13,7 +13,12 @@ const CACHE_URLS = [
   self.location.origin + '/images/favicons/icon-96x96.png',
   self.location.origin + '/images/favicons/icon-144x144.png',
   self.location.origin + '/images/favicons/icon-192x192.png',
-  self.location.origin + '/images/logo.min.svg'
+  self.location.origin + '/images/logo.min.svg',
+  self.location.origin + '/favicon.ico',
+  self.location.origin + '/editor.html',
+  self.location.origin + '/script/editor/main.min.js',
+  self.location.origin + '/style/editor.css',
+  self.location.origin + '/deleted.html'
 ];
 
 var cmd = {
