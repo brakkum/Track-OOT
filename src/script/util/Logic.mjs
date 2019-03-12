461ef513e73f8ca768247efdb57444d4aae2ccf8
Denis Peters <denis.peters@udo.edu>
2019-03-12 18:32:47 +0100

    move logic type decision to LogicWrapper

--- src/script/util/Logic.mjs
+++ src/script/util/Logic.mjs
@@ -1,49 +1,32 @@
-import DeepLogicAbstractElement from "/deepJS/ui/logic/elements/LogicAbstractElement.mjs";
-import GlobalData from "/deepJS/storage/GlobalData.mjs";
 import LogicWrapper from "/script/util/LogicWrapper.mjs";
 
-const DEFAULT_LOGIC = {
-    chests: new Map,
-    skulltulas: new Map,
-    mixins: new Map
-};
-const CUSTOM_LOGIC  = {
-    chests: new Map,
-    skulltulas: new Map,
-    mixins: new Map
+const LOGIC = {
+    chests: {},
+    skulltulas: {},
+    hintstones: {},
+    mixins: {}
 };
 
 class TrackerLogic {
 
-    loadLogic() {
-        let logic = GlobalData.get("logic");
+    loadLogic() { // TODO get location list from locations file
         for (let i of logic.chests) {
-            DEFAULT_LOGIC.chests.set(i, new (LogicWrapper("chests", i, logic))); // TODO apply this to the rest
+            LOGIC.chests[i] = new LogicWrapper("chests", i);
         }
         for (let i of logic.skulltulas) {
-            DEFAULT_LOGIC.skulltulas.set(i, DeepLogicAbstractElement.buildLogic(logic.skulltulas[i]));
+            LOGIC.skulltulas[i] = new LogicWrapper("skulltulas", i);
+        }
+        for (let i of logic.hintstones) {
+            LOGIC.hintstones[i] = new LogicWrapper("hintstones", i);
         }
         for (let i of logic.mixins) {
-            DEFAULT_LOGIC.mixins.set(i, DeepLogicAbstractElement.buildLogic(logic.mixins[i]));
-        }
-        let patched_logic = GlobalData.get("logic_patched");
-        for (let i of patched_logic.chests) {
-            CUSTOM_LOGIC.chests.set(i, DeepLogicAbstractElement.buildLogic(patched_logic.chests[i]));
-        }
-        for (let i of patched_logic.skulltulas) {
-            CUSTOM_LOGIC.skulltulas.set(i, DeepLogicAbstractElement.buildLogic(patched_logic.skulltulas[i]));
-        }
-        for (let i of patched_logic.mixins) {
-            CUSTOM_LOGIC.mixins.set(i, DeepLogicAbstractElement.buildLogic(patched_logic.mixins[i]));
+            LOGIC.mixins.set[i] = new LogicWrapper("mixins", i);
         }
     }
 
     getValue(type, ref) {
-        if (CUSTOM_LOGIC[type].has(ref)) {
-            return CUSTOM_LOGIC[type].get(ref).value;
-        }
-        if (DEFAULT_LOGIC[type].has(ref)) {
-            return DEFAULT_LOGIC[type].get(ref).value;
+        if (!!LOGIC[type] && !!LOGIC[type][ref]) {
+            return LOGIC[type][ref].value;
         }
         return false;
     }
--- src/script/util/LogicWrapper.mjs
+++ src/script/util/LogicWrapper.mjs
@@ -1,17 +1,42 @@
+
+import DeepLocalStorage from "/deepJS/storage/LocalStorage.mjs";
+import GlobalData from "/deepJS/storage/GlobalData.mjs";
 import EventBus from "/deepJS/util/EventBus.mjs";
 
-const LOGIC = new WeakMap;
+const LOGIC_DEFAULT = new WeakMap;
+const LOGIC_CUSTOM = new WeakMap;
 const VALUE = new WeakMap;
+
+const INSTANCES = new WeakSet;
+
+function buildDefaultLogic(logic, type, ref) {
+    let build = DeepLogicAbstractElement.buildLogic(logic[type][ref]);
+    build.onupdate = v => {
+        if (!DeepLocalStorage.get("settings", "use_custom_logic", false)) {
+            this.value = v;
+            EventBus.post("logic", type, ref, v);
+        }
+    };
+    return build;
+}
+
+function buildCustomLogic(logic, type, ref) {
+    let build = DeepLogicAbstractElement.buildLogic(logic[type][ref]);
+    build.onupdate = v => {
+        if (DeepLocalStorage.get("settings", "use_custom_logic", false)) {
+            this.value = v;
+            EventBus.post("logic", type, ref, v);
+        }
+    };
+    return build;
+}
 
 export default class LogicWrapper {
 
-    constructor(type, ref, logic) {
-        let build = DeepLogicAbstractElement.buildLogic(logic[type][ref]);
-        build.onupdate = v => {
-            this.value = v;
-            EventBus.post("logic", type, ref, v);
-        };
-        LOGIC.set(this, build);
+    constructor(type, ref) {
+        LOGIC_DEFAULT.set(this, buildDefaultLogic.apply(this, GlobalData.get("logic"), type, ref, false));
+        LOGIC_CUSTOM.set(this, buildCustomLogic.apply(this, GlobalData.get("logic_patched"), type, ref, true));
+        INSTANCES.add(this);
     }
 
     set value(value) {
@@ -25,4 +50,19 @@ export default class LogicWrapper {
         return false;
     }
 
+    loadCustomLogic() {
+        if (DeepLocalStorage.get("settings", "use_custom_logic", false)) {
+            let oldValue = GlobalData.get("logic_patched", {});
+            let newValue = DeepLocalStorage.get("settings", "logic", {});
+            if (JSON.stringify(oldValue) == JSON.stringify(newValue)) {
+                GlobalData.set("logic_patched", newValue);
+                
+            }
+        }
+    }
+
+}
+
+window.onfocus = function(ev) {
+    
 }
