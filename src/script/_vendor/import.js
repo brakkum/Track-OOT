8fcdc728b9257194788c50d948aa04c8f51a0214
Denis Peters <denis.peters@udo.edu>
2019-04-04 19:28:20 +0200

    better error handling

--- src/script/_vendor/import.js
+++ src/script/_vendor/import.js
@@ -1,13 +1,13 @@
 !function() {
 
-    function getFile(url) {
-        return fetch(url)
-            .then(function(r) {
-                if (r.status < 200 || r.status >= 300) {
-                    throw new Error(`error loading file "${url}" - status: ${r.status}`);
-                }
-                return r;
-            });
+    const PARSER = new DOMParser();
+
+    async function getFile(url) {
+        let r = await fetch(url);
+        if (r.status < 200 || r.status >= 300) {
+            throw new Error(`error loading file "${url}" - status: ${r.status}`);
+        }
+        return r;
     }
 
     class Importer {
@@ -15,14 +15,8 @@
         importHTML(url) {
             return getFile(url)
                 .then(r => r.text())
-                .then(r => (new DOMParser()).parseFromString(r, "text/html"))
-                .then(r => {
-                    while (r.body.childNodes.length > 0) {
-                        document.body.appendChild(r.body.childNodes[0]);
-                    }
-                }).catch(e => {
-                    updateLoadingMessage(e);
-                });
+                .then(r => PARSER.parseFromString(r, "text/html"))
+                .then(r => r.body.childNodes);
         }
 
         importCSS(url) {
@@ -34,11 +28,11 @@
                 t.onload = function() {
                     res(t);
                 };
-                t.onerror = function(e) {
+                t.onerror = function() {
                     getFile(url).then(function() {
                         rej(`error appending style "${url}"`);
-                    }, function(r) {
-                        rej(r);
+                    }, function(e) {
+                        rej(e);
                     })
                 };
                 document.head.appendChild(t);
@@ -55,9 +49,9 @@
                 };
                 t.onerror = function(e) {
                     getFile(url).then(function() {
-                        rej(`error appending style "${url}"`);
-                    }, function(r) {
-                        rej(r);
+                        rej(`error appending module "${url}"`);
+                    }, function(e) {
+                        rej(e);
                     })
                 };
                 document.head.appendChild(t);
