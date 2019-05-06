145c394c13b218d24d90175904d7ac530917fe99
Denis Peters <denis.peters@udo.edu>
2019-05-06 22:15:39 +0200

    fix rtc url config

--- src/script/client/RTCClient.mjs
+++ src/script/client/RTCClient.mjs
@@ -8,19 +8,15 @@ const ON_DISCONNECTED = new WeakMap();
 
 const configuration = {
     iceServers: [{
-        urls: [
-            'stun:stun.l.google.com',
-            {
-                url: 'turn:192.158.29.39:3478?transport=udp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            },
-            {
-                url: 'turn:192.158.29.39:3478?transport=tcp',
-                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                username: '28224511:1379330808'
-            },
-        ]
+        urls: 'stun:stun.l.google.com'
+    }, {
+        urls: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+    }, {
+        urls: 'turn:192.158.29.39:3478?transport=tcp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
     }]
 };
 
