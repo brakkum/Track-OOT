71e26be8d9bf2a2ce70efe6dc3376b97697f2859
Denis Peters <denis.peters@udo.edu>
2019-05-06 22:11:33 +0200

    add credentials for turn

--- src/script/client/RTCHost.mjs
+++ src/script/client/RTCHost.mjs
@@ -13,8 +13,16 @@ const configuration = {
     iceServers: [{
         urls: [
             'stun:stun.l.google.com',
-            'turn:turn01.hubl.in?transport=udp',
-            'turn:turn02.hubl.in?transport=tcp'
+            {
+                url: 'turn:192.158.29.39:3478?transport=udp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+            },
+            {
+                url: 'turn:192.158.29.39:3478?transport=tcp',
+                credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                username: '28224511:1379330808'
+            },
         ]
     }]
 };
