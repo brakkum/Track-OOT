db0d4ee412e8de9c5f1b3a1e9035da9be678fbd5
Denis Peters <denis.peters@udo.edu>
2019-08-23 20:09:33 +0200

    write lastchanged date to state saving; save state to localstorage buffer as full object; add autosave skeleton;

--- src/script/storage/SaveState.js
+++ src/script/storage/SaveState.js
@@ -3,27 +3,39 @@ import Logger from "/deepJS/util/Logger.js";
 import TrackerStorage from "/script/storage/TrackerStorage.js";
 
 !function() {
+    let tmp = {};
     let k = Object.keys(sessionStorage);
     for (let i of k) {
         let r = i.split("\0");
         if (r[0] != "meta") {
             if (r[0] == "extras") {
-                localStorage.setItem(r[1], sessionStorage.getItem(i));
+                tmp[r[1]] = sessionStorage.getItem(i);
             } else {
-                localStorage.setItem(`${r[0]}.${r[1]}`, sessionStorage.getItem(i));
+                tmp[`${r[0]}.${r[1]}`] = sessionStorage.getItem(i);
             }
         } else {
-            localStorage.setItem("name", sessionStorage.getItem("meta\0active_state"));
+            tmp.name = sessionStorage.getItem("meta\0active_state");
         }
     }
+    LocalStorage.set("savestate", tmp);
     sessionStorage.clear();
 }();
 
-let state = LocalStorage.getAll();
+// TODO make autosave be called every X min, store last Y saves. if manual save restart timer to X. autosave to be stored to LocalStorage
+function autosave() {
+    // TODO remove oldest autosave while autosave count >= Y
+    // TODO store new autosave
+    Logger.info(`saved state as "${name}"`, "SaveState");
+    // TODO call timer with X seconds to autosave
+}
+// TODO call timer with X seconds to autosave
+
+let state = LocalStorage.get("savestate", {});
 
 class SaveState {
 
     async save(name) {
+		state.lastchanged = new Date();
         await TrackerStorage.StatesStorage.set(name, state);
         Logger.info(`saved state as "${name}"`, "SaveState");
     }
@@ -50,7 +62,7 @@ class SaveState {
 
     write(key, value) {
         state[key] = value;
-        LocalStorage.set(key, value);
+        LocalStorage.set("savestate", state);
     }
 
     read(key, def) {
@@ -63,13 +75,13 @@ class SaveState {
     remove(key) {
         if (!!state.hasOwnProperty(key)) {
             delete state[key];
-            LocalStorage.delete(key);
+            LocalStorage.set("savestate", state);
         }
     }
 
     reset() {
         state = {};
-        LocalStorage.clear();
+        LocalStorage.set("savestate", state);
         Logger.info(`state resettet`, "SaveState");
     }
 
