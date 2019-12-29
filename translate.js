a0578f54fb032bf3b28624e79d4bfac4d31914c2
Denis Peters <denis.peters@udo.edu>
2019-12-29 22:11:08 +0100

    added old locations file with some changes to structure;

--- translate.js
+++ translate.js
@@ -20,82 +20,78 @@ const PROPERTY_FILES = [
 
 let translation = {};
 
-!function() { /* location-converter */
 
-    // TODO: filter position data (x/y) and other metadata
-    // TODO: check why skulltulas are not extracted properly
-    // TODO: maybe check for the 5 kinds directly instead of dynamically
+// TODO: filter position data (x/y) and other metadata
+// TODO: check why skulltulas are not extracted properly
 
-    const LOCATION_STRUCT = {
-        "type": "chest",
-        "mode": "all",
-        "era": "both",
-        "time": "always"
+const LOCATION_STRUCT = {
+    "type": "chest",
+    "mode": "all",
+    "era": "both",
+    "time": "always"
+}
+let locations = JSON.parse(fs.readFileSync("./src/database/locations.json"));
+let converted_locations = {};
+
+function convert_locations(area, name, type, data) {
+    let record = Object.assign({}, LOCATION_STRUCT);
+    record.type = type.replace(/s$/, "");
+    if (!!data.mode) {
+        record.mode = data.mode;
+        record.type = data.mode.slice(0, -6);
     }
-    let locations = JSON.parse(fs.readFileSync("./src/database/locations.json"));
-    let converted = {};
-
-    function convert(area, name, type, data) {
-        let record = Object.assign({}, LOCATION_STRUCT);
-        record.type = type.replace(/s$/, "");
-        if (!!data.mode) {
-            record.mode = data.mode;
-            record.type = data.mode.slice(0, -6);
-        }
-        if (!!data.era) {
-            record.era = data.era;
-        }
-        if (!!data.time) {
-            record.time = data.time;
-        }
-        if (!!data.counts && data.counts > 1) {
-            let trans = [];
-            for (let l = 0; l < data.counts; ++l) {
-                if (converted.hasOwnProperty(`location.${area}.${name}_${l}`)) {
-                    console.error(`name duplication: location.${area}.${name}_${l}`);
-                }
-                converted[`location.${area}.${name}_${l}`] = record;
-                trans.push(`location.${area}.${name}_${l}`);
-            }
-            translation[`${type}.${name}`] = trans;
-        } else {
-            if (converted.hasOwnProperty(`location.${area}.${name}`)) {
-                console.error(`name duplication: location.${area}.${name}`);
-            }
-            converted[`location.${area}.${name}`] = record;
-            translation[`${type}.${name}`] = `location.${area}.${name}`;
-        }
+    if (!!data.era) {
+        record.era = data.era;
     }
+    if (!!data.time) {
+        record.time = data.time;
+    }
+    if (!!data.counts && data.counts > 1) {
+        let trans = [];
+        for (let l = 0; l < data.counts; ++l) {
+            if (converted_locations.hasOwnProperty(`location.${area}.${name}_${l}`)) {
+                console.error(`name duplication: location.${area}.${name}_${l}`);
+            }
+            converted_locations[`location.${area}.${name}_${l}`] = record;
+            trans.push(`location.${area}.${name}_${l}`);
+        }
+        translation[`${type}.${name}`] = trans;
+    } else {
+        if (converted_locations.hasOwnProperty(`location.${area}.${name}`)) {
+            console.error(`name duplication: location.${area}.${name}`);
+        }
+        converted_locations[`location.${area}.${name}`] = record;
+        translation[`${type}.${name}`] = `location.${area}.${name}`;
+    }
+}
 
-    for (let i in locations) {
-        if (locations[i].hasOwnProperty("chests_v")) {
-            for (let k in locations[i].chests_v) {
-                convert(i, k, "chests", locations[i].chests_v[k]);
-            }
-        }
-        if (locations[i].hasOwnProperty("chests_mq")) {
-            for (let k in locations[i].chests_mq) {
-                convert(`${i}_mq`, k, "chests", locations[i].chests_mq[k]);
-            }
-        }
-        if (locations[i].hasOwnProperty("skulltulas_v")) {
-            for (let k in locations[i].skulltulas_v) {
-                convert(i, k, "skulltulas", locations[i].skulltulas_v[k]);
-            }
-        }
-        if (locations[i].hasOwnProperty("skulltulas_mq")) {
-            for (let k in locations[i].skulltulas_mq) {
-                convert(`${i}_mq`, k, "skulltulas", locations[i].skulltulas_mq[k]);
-            }
-        }
-        if (locations[i].hasOwnProperty("gossipstones_v")) {
-            for (let k in locations[i].gossipstones_v) {
-                convert(i, k, "gossipstones", locations[i].gossipstones_v[k]);
-            }
+for (let i in locations) {
+    if (locations[i].hasOwnProperty("chests_v")) {
+        for (let k in locations[i].chests_v) {
+            convert_locations(i, k, "chests", locations[i].chests_v[k]);
         }
     }
+    if (locations[i].hasOwnProperty("chests_mq")) {
+        for (let k in locations[i].chests_mq) {
+            convert_locations(`${i}_mq`, k, "chests", locations[i].chests_mq[k]);
+        }
+    }
+    if (locations[i].hasOwnProperty("skulltulas_v")) {
+        for (let k in locations[i].skulltulas_v) {
+            convert_locations(i, k, "skulltulas", locations[i].skulltulas_v[k]);
+        }
+    }
+    if (locations[i].hasOwnProperty("skulltulas_mq")) {
+        for (let k in locations[i].skulltulas_mq) {
+            convert_locations(`${i}_mq`, k, "skulltulas", locations[i].skulltulas_mq[k]);
+        }
+    }
+    if (locations[i].hasOwnProperty("gossipstones_v")) {
+        for (let k in locations[i].gossipstones_v) {
+            convert_locations(i, k, "gossipstones", locations[i].gossipstones_v[k]);
+        }
+    }
+}
 
-    fs.writeFileSync("./src/database/rework/locations.json", JSON.stringify(converted, null, 4));
-}();
-
+fs.writeFileSync("./src/database/rework/locations.json", JSON.stringify(converted_locations, null, 4));
 fs.writeFileSync("./src/database/rework/translation.json", JSON.stringify(translation, null, 4));
