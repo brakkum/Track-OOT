3c9bfef7b477fb8973a400a76fc63563030ab87e
Denis Peters <denis.peters@udo.edu>
2020-05-10 19:25:30 +0200

    fix stun server config

--- src/script/util/RTCController.js
+++ src/script/util/RTCController.js
@@ -9,11 +9,11 @@ const CONFIG = [{
     urls: 'stun:stun.zidargs.net:18001'
 },{
     urls: [
-        'stun.l.google.com:19302',
-        'stun1.l.google.com:19302',
-        'stun2.l.google.com:19302',
-        'stun3.l.google.com:19302',
-        'stun4.l.google.com:19302'
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun3.l.google.com:19302',
+        'stun:stun4.l.google.com:19302'
     ]
 },{
     urls: 'turn:turn.zidargs.net:18001',
