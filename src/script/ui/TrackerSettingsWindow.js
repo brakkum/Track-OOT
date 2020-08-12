103e1a53f36dd85b3df94dfa8ae22d16a37d3b24
Denis Peters <denis.peters@udo.edu>
2020-08-12 05:24:06 +0200

    recreate the credits;

--- src/script/ui/TrackerSettingsWindow.js
+++ src/script/ui/TrackerSettingsWindow.js
@@ -22,6 +22,8 @@ const settings = new SettingsWindow;
 
 BusyIndicator.setIndicator(document.getElementById("busy-animation"));
 
+const SUPPORTER_URL = new URL("/patreon", location);
+
 const ABOUT_TPL = new Template(`
 <div style="display: flex; margin-bottom: 10px;">
     <div style="flex: 1">
@@ -48,24 +50,91 @@ const ABOUT_TPL = new Template(`
     If the tracker acts weird, please <a href="https://bitbucket.org/zidargs/track-oot/issues" target="_blank" rel="noreferrer">report the error!</a><br><br>
     You can also report via Discord ▶ <a href="https://discord.gg/wgFVtuv" target="_blank" rel="noreferrer">Join my Discord!</a><br><br>
 </div>
-<hr>
-<div>
-Big thanks to:<br>
-<i class="thanks-name">TestRunner</i> for creating the original tracker.<br>
-<i class="thanks-name">Scatter</i> for building a logic compendium.<br>
-<i class="thanks-name">fraggerman</i> for helping with the logic.<br>
-<i class="thanks-name">Luigimeansme</i> for helping with adding Master Quest.<br>
-<i class="thanks-name">pidgezero_one</i> for adding sequence breaks and extending skips.
+`);
+
+const CREDITS_TPL = new Template(`
+<style>
+    #credits {
+        height: 400px;
+        display: flex;
+        color: white;
+        overflow: hidden;
+    }
+    #credits .panel {
+        display: block;
+        flex: 1;
+        padding: 10px;
+        margin: 1px;
+        background-color: #282828;
+    }
+    #credits ul {
+        padding-inline-start: 10px;
+    }
+    #credits .title {
+        font-size: 1.2em;
+        font-weight: bold;
+    }
+    #credits .name {
+        list-style: none;
+        padding: 5px 0;
+        font-size: 1.5em;
+    }
+    #credits .name.owner {
+        color: #cb9c3d;
+    }
+    #credits .name.developer {
+        color: #ffdb00;
+    }
+    #credits .name.contributor {
+        color: #ff2baa;
+    }
+    #credits #supporters .name {
+        color: #a8a8a8;
+    }
+</style>
+<div id="credits">
+    <div class="panel">
+        <label>
+            <span class="title">Dev-Team</span>
+            <ul>
+                <li class="name owner">ZidArgs</li>
+                <li class="name developer">fraggerman</li>
+            </ul>
+        </label>
+        <label>
+            <span class="title">Contributors</span>
+            <ul>
+                <li class="name contributor">Luigimeansme</li>
+                <li class="name contributor">pidgezero_one</li>
+                <li class="name contributor">Elagatua</li>
+                <li class="name contributor">Takacomic</li>
+            </ul>
+        </label>
+    </div>
+    <div class="panel" id="supporters">
+    </div>
 </div>
 `);
 
-const LOAD_RULESET = new Template(`
-<label class="settings-option">
-    <span id="load-template-title" class="option-text"></span>
-    <button id="load-template-button" class="settings-button" type="button" value="undefined" style="margin-right: 10px;"></button>
-    <select id="select-template" class="settings-input" type="input"></select>
-</label>
-`)
+function createSupporterPanel(title, data) {
+    if (data != null && Array.isArray(data.names) && data.names.length > 0) {
+        let res = document.createElement("label");
+        let ttl = document.createElement("span");
+        ttl.classList.add("title");
+        ttl.innerHTML = title;
+        let lst = document.createElement("ul");
+        for (let name of new Set(data.names)) {
+            let el = document.createElement("li");
+            el.classList.add("name");
+            el.innerHTML = name;
+            el.style.color = data.color || "";
+            lst.append(el);
+        }
+        res.append(ttl);
+        res.append(lst);
+        return res;
+    }
+};
 
 async function getSettings() {
     let options = FileData.get("settings");
@@ -128,6 +197,21 @@ export default class Settings {
         });
         settings.addTab("About", "about");
         settings.addElements("about", settings_about);
+    
+        !async function() {
+            let settings_credits = CREDITS_TPL.generate();
+            let supporters_list = settings_credits.getElementById("supporters");
+            let supporters = await fetch(SUPPORTER_URL).then(r=>r.json());
+            for (let name in supporters) {
+                let el = createSupporterPanel(name, supporters[name]);
+                if (el != null) {
+                    supporters_list.append(el);
+                }
+            }
+            settings.addTab("Credits", "credits");
+            settings.addElements("credits", settings_credits);
+        }();
+        
 
         settings.addEventListener('submit', function(event) {
             BusyIndicator.busy();
