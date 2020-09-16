7ba1c149bbab984b6a4b8cd472639d20a99b339c
Denis Peters <denis.peters@udo.edu>
2020-09-16 23:58:09 +0200

    add interiors and grottos to exits; use new logic with gateway nodes for dungeons;

--- src/script/storage/converters/StateConverter4.js
+++ src/script/storage/converters/StateConverter4.js
@@ -1,3 +1,17 @@
+const EXIT_TRANS = {
+    "region.deku_tree_lobby": "region.deku_tree_gateway",
+    "region.dodongos_cavern_beginning": "region.dodongos_cavern_gateway",
+    "region.jabu_jabus_belly_beginning": "region.jabu_jabus_belly_gateway",
+    "region.bottom_of_the_well": "region.bottom_of_the_well_gateway",
+    "region.forest_temple_lobby": "region.forest_temple_gateway",
+    "region.fire_temple_lower": "region.fire_temple_gateway",
+    "region.water_temple_lobby": "region.water_temple_gateway",
+    "region.spirit_temple_lobby": "region.spirit_temple_gateway",
+    "region.shadow_temple_entryway": "region.shadow_temple_gateway",
+    "region.ice_cavern_beginning": "region.ice_cavern_gateway",
+    "region.gerudo_training_grounds_lobby": "region.gerudo_training_grounds_gateway"
+};
+
 export default function(state) {
     let res = {
         data: {},
@@ -40,6 +54,15 @@ export default function(state) {
     }
     if (res.data["option.doors_open_forest"] === false) {
         res.data["option.doors_open_forest"] = "doors_open_forest_closed";
+    }
+    if (state.extra.exits != null) {
+        let buf = {};
+        for (let i of Object.keys(state.extra.exits)) {
+            let [k1, k2] = i.split(" -> ");
+            let [v1, v2] = state.extra.exits[i].split(" -> ");
+            buf[`${EXIT_TRANS[k1] || k1} -> ${EXIT_TRANS[k2] || k2}`] = `${EXIT_TRANS[v1] || v1} -> ${EXIT_TRANS[v2] || v2}`;
+        }
+        res.extra.exits = buf;
     }
     return res;
 };
