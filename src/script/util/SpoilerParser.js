2ccd6b6d89b88269c983fd648e028df34defa42e
trulytaka <trulyjustandrew@gmail.com>
2020-10-08 19:40:50 -0700

    fixed bug in trials and gerudo fortress spoiler reading; added new TODOs; added entrances to spoiler reader

--- src/script/util/SpoilerParser.js
+++ src/script/util/SpoilerParser.js
@@ -1,5 +1,7 @@
 import StateStorage from "/script/storage/StateStorage.js";
 import FileData from "/emcJS/storage/FileData.js";
+import EventBus from "/emcJS/util/events/EventBus.js";
+
 let trans
 let options = {};
 
@@ -13,7 +15,7 @@ function versionChecker(version) {
     return failure
 }
 
-function parseSetting(setting) {
+function parseSetting(setting, world) {
     let setting_trans = trans["setting"];
     for(let i in setting) {
         let v = setting[i];
@@ -22,13 +24,13 @@ function parseSetting(setting) {
             if(Array.isArray(v)) {
                 v = new Set(v);
                 setting_trans[i].forEach(el => {
-                    options[el.replace("logic_", "skip.")] = v.has(el);
+                    options[world][el.replace("logic_", "skip.")] = v.has(el);
                 });
             } else {
-                options[setting_trans[i]["name"]] = setting_trans[i]["values"][v];
+                options[world][setting_trans[i]["name"]] = setting_trans[i]["values"][v];
 
-                if(setting_trans[i] === "shuffle_ganon_bosskey" && v === "remove") options["option.ganon_boss_door_open"] = true;
-                if(setting_trans[i]["values"][v] === undefined) console.warn(i + ": " + v + "    is a invalid value. Please report this bug")
+                if(setting_trans[i] === "shuffle_ganon_bosskey" && v === "remove") options[world]["option.ganon_boss_door_open"] = true;
+                if(setting_trans[i]["values"][v] === undefined) console.warn("[" + i + ": " + v + "] is a invalid value. Please report this bug")
             }
         }
     }
@@ -37,6 +39,8 @@ function parseSetting(setting) {
 function parseStartingItems(items, world) {
     let starting_trans = trans["starting_items"];
     let bottles = 0;
+    let bottle;
+    let bottleWL;
 
     for(let w = 1; w <= world; w++) {
         if(world !== 1) items = items["World " + w];
@@ -46,17 +50,41 @@ function parseStartingItems(items, world) {
 
             if(starting_trans.hasOwnProperty(i)) {
                 if(Array.isArray(i)) {
-                    console.warn("Unexpected Array withing starting items, please report this!")
-                    break;
-                }
+                    console.warn("Unexpected Array within starting items, please report this!")
+                } else {
+                    if (starting_trans[i]["values"][v] === undefined) {
+                        console.warn("[" + i + ": " + v + "] is a invalid value. Please report this bug")
+                    } else {
+                        options[w][starting_trans[i]["name"]] = starting_trans[i]["values"][v];
 
-                options[starting_trans[i]["name"]] = starting_trans[i]["values"][v];
+                        if (i !== "Bottle") {
+                            EventBus.trigger("item", {
+                                name: starting_trans[i]["name"],
+                                value: starting_trans[i]["values"][v]
+                            });
+                        }
 
-                if(i === "Bottle With Letter" || i === "Bottle") {
-                    bottles = bottles + starting_trans[i]["values"][v]
-                    options[starting_trans["Bottle"]["name"]] = bottles;
+                        if (i === "Bottle With Letter" || i === "Bottle") {
+                            if(i === "Bottle With Letter") bottleWL = true;
+                            if(i === "Bottle") bottle = true;
+
+                            bottles = bottles + starting_trans[i]["values"][v]
+                            options[w][starting_trans["Bottle"]["name"]] = bottles;
+                            if(i.hasOwnProperty("Bottle") && i.hasOwnProperty("Bottle With Letter") && bottle && bottleWL) {
+                                EventBus.trigger("item", {
+                                    name: starting_trans["Bottle"]["name"],
+                                    value: bottles
+                                });
+                            }
+                            if(!i.hasOwnProperty("Bottle") || !i.hasOwnProperty("Bottle With Letter")) {
+                                EventBus.trigger("item", {
+                                    name: starting_trans["Bottle"]["name"],
+                                    value: bottles
+                                });
+                            }
+                        }
+                    }
                 }
-                if(starting_trans[i]["values"][v] === undefined) console.warn(i + ": " + v + "    is a invalid value. Please report this bug")
             }
         }
     }
@@ -66,18 +94,24 @@ function parseDungeons(dungeons, world) {
     let dungeon_trans = trans["dungeons"];
 
     for(let w = 1; w <= world; w++) {
-        if (world !== 1) dungeons = dungeons["World " + w];
+        if(world !== 1) dungeons = dungeons["World " + w];
 
         for(let i in dungeons) {
             let v = dungeons[i];
             if(dungeon_trans.hasOwnProperty(i)) {
                 if(Array.isArray(i)) {
-                    console.warn("Unexpected Array withing starting items, please report this!")
-                    break;
+                    console.warn("Unexpected Array within dungeon types, please report this!")
+                } else {
+                    if (dungeon_trans[i]["values"][v] === undefined) {
+                        console.warn("[" + i + ": " + v + "] is a invalid value. Please report this bug")
+                    } else {
+                        options[w][dungeon_trans[i]["name"]] = dungeon_trans[i]["values"][v];
+                        EventBus.trigger("dungeontype", {
+                            name: dungeon_trans[i]["name"].replace("dungeonTypes.", ""),
+                            value: dungeon_trans[i]["values"][v]
+                        });
+                    }
                 }
-
-                options[dungeon_trans[i]["name"]] = dungeon_trans[i]["values"][v];
-                if(dungeon_trans[i]["values"][v] === undefined) console.warn(i + ": " + v + "    is a invalid value. Please report this bug")
             }
         }
     }
@@ -87,23 +121,53 @@ function parseTrials(trials, world) {
     let trial_trans = trans["trials"];
 
     for(let w = 1; w <= world; w++) {
-        if (world !== 1) trials = trials["World " + w];
+        if(world !== 1) trials = trials["World " + w];
 
         for(let i in trials) {
             let v = trials[i];
             if(trial_trans.hasOwnProperty(i)) {
                 if(Array.isArray(i)) {
-                    console.warn("Unexpected Array withing starting items, please report this!")
-                    break;
+                    console.warn("Unexpected Array within active trials, please report this!")
+                } else {
+                    if(trial_trans[i]["values"][v] === undefined) {
+                        console.warn("[" + i + ": " + v + "] is a invalid value. Please report this bug")
+                    } else {
+                        options[w][trial_trans[i]["name"]] = trial_trans[i]["values"][v];
+                    }
                 }
-
-                options[trial_trans[i]["name"]] = trial_trans[i]["values"][v];
-                if(trial_trans[i]["values"][v] === undefined) console.warn(i + ": " + v + "    is a invalid value. Please report this bug")
             }
         }
     }
 }
 
+function parseEntrances(entrances, world, dungeon, simple, indoors, overworld) {
+    let [entrance_trans, exit_trans] = trans["entrances"];
+    let [entro_dungeon, entro_simple, entro_indoors, entro_overworld] = entrance_trans;
+    let [exit_dungeon, exit_simple, exit_indoors, exit_overworld] = exit_trans;
+
+    for(let w = 1; w < world; w++) {
+        if(world !== 1) entrances =  entrances["World " + w];
+
+        for(let i in entrances) {
+            let v = entrances[i];
+            if(entrance_trans.hasOwnProperty(i)) {
+                if(Array.isArray(i)) {
+                    console.warn("Unexpected Array within entrances, please report this!")
+                } else {
+                    if(Array.isArray(v)) {
+                        v = v["region"] + " -> " + v["from"];
+                    } else {
+                        if (entrance_trans[i]["values"][v] === undefined) {
+                            console.warn("[" + i + ": " + v + "] is a invalid value. Please report this bug")
+                        } else {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 function parseLocations(locations, world) {
     // TODO add code to parse location items to tracker locations with Item Association
@@ -118,29 +182,29 @@ class SpoilerParser {
     parse(spoiler, settings) {
         let data = spoiler;
         trans = FileData.get("options_trans")
+        let multiWorld = settings["parse.multiworld"];
 
         let version = versionChecker(data[":version"]);
         if(version === 0) return console.log("Fail State: Not a valid OOTR Spoiler log");
         if(version >= 1) {
             let world = 0;
             if(!isNaN(data["settings"]["world_count"])) world = data["settings"]["world_count"];
+            if(world === 1) multiWorld = world;
 
-            if(settings["parse.settings"]) parseSetting(data["settings"])
-            if(world === 1) {
-                if(settings["parse.starting_items"]) parseStartingItems(data["starting_items"], world);
-                //if(settings["parse.item_association"]) parseLocations(data["locations"], world);
-                //if(settings["parse.woth_hints"]) parseWothLocation(data[":woth_locations"], world);
-                //if(settings["parse.entrances"]) parseEntrances(data["entrances"], world);
-                //if(settings["parse.shops"]) parseShops(data["locations"], world);
-                //if(settings["parse.gossip_stones"]) parseStones(data["gossip_stones"], world);
-                //if(settings["parse.barren"]) parseBarren(data[":barren_regions"], world);
-                if(settings["parse.trials"]) parseTrials(data["trails"], world);
-                //if(settings["parse.random_settings"]) parseRandom(data["randomized_settings"], world);
-                if(settings["parse.dungeons"]) parseDungeons(data["dungeons"], world);
-            } else {
-                console.log("Multiworld location parsing currently not supported")
-            }
-            StateStorage.write(options);
+            if(settings["parse.settings"]) parseSetting(data["settings"], multiWorld)
+            if(settings["parse.starting_items"]) parseStartingItems(data["starting_items"], world);
+            //if(settings["parse.item_association"]) parseLocations(data["locations"], world);
+            //if(settings["parse.woth_hints"]) parseWothLocation(data[":woth_locations"], world);
+            if(settings["parse.entrances"]) parseEntrances(data["entrances"], world);
+            //if(settings["parse.shops"]) parseShops(data["locations"], world);
+            //if(settings["parse.gossip_stones"]) parseStones(data["gossip_stones"], world);
+            //if(settings["parse.barren"]) parseBarren(data[":barren_regions"], world);
+            if(settings["parse.trials"]) parseTrials(data["trials"], world);
+            //if(settings["parse.random_settings"]) parseRandom(data["randomized_settings"], world);
+            if(settings["parse.dungeons"]) parseDungeons(data["dungeons"], world);
+
+            StateStorage.write(options[multiWorld]);
+            EventBus.trigger("randomizer_options", options);
         }
     }
 }
