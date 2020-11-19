5f0309bbb967b5364acc74668dc8917d9774891b
Denis Peters <denis.peters@udo.edu>
2020-11-19 18:15:20 +0100

    fixed key counting issue for V/MQ dungeons; fixed pocket reward marker in item grid;

---
+++ src/script/storage/converter/StateConverter11.js
@@ -0,0 +1,37 @@
+/**
+ * move to serverside past TBD
+ */
+
+import StateConverter from "../StateConverter.js";
+
+StateConverter.register(function(state) {
+    let res = {
+        data: state.data,
+        extra: {},
+        notes: state.notes,
+        autosave: state.autosave,
+        timestamp: state.timestamp,
+        name: state.name
+    };
+    // change dungeonreward
+    const dungeonreward = {};
+    if (state.extra.dungeonreward != null) {
+        for (let i of Object.keys(state.extra.dungeonreward)) {
+            dungeonreward[translation[i] || i] = translation[state.extra.dungeonreward[i]] || state.extra.dungeonreward[i];
+        }
+    }
+    // change dungeontype
+    const dungeontype = {};
+    if (state.extra.dungeontype != null) {
+        for (let i of Object.keys(state.extra.dungeontype)) {
+            dungeontype[translation[i] || i] = translation[state.extra.dungeontype[i]] || state.extra.dungeontype[i];
+        }
+    }
+    // collect data
+    res.extra = {...state.extra, dungeonreward, dungeontype};
+    return res;
+});
+
+const translation = {
+    "pocket": "area/pocket",
+};
